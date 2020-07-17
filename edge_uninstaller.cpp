b0b2c61e577ac46a818e6730b0b16971b1b35020
Vichhika <51940586+vichhika@users.noreply.github.com>
2020-07-17 17:14:32 +0700

    Update edge_uninstaller.cpp

--- edge_uninstaller.cpp
+++ edge_uninstaller.cpp
@@ -6,16 +6,12 @@ string exec(string command) {
    char buffer[128];
    string result = "";
 
-   // Open pipe to file
    FILE* pipe = popen(command.c_str(), "r");
    if (!pipe) {
       return "popen failed!";
    }
 
-   // read till end of process:
    while (!feof(pipe)) {
-
-      // use buffer to read and add to result
       if (fgets(buffer, 128, pipe) != NULL)
          result += buffer;
    }
@@ -35,13 +31,3 @@ int main(){
     }
     return 0;
 }
-
-
-
-/* reference
-https://stackoverflow.com/questions/15141536/c-alternative-to-os-walk
-https://ss64.com/nt/dir.html
-http://www.davismol.net/2012/04/07/dos-list-and-filter-files-in-a-folder-with-dir-and-findstr-commands/
-https://www.shellhacks.com/windows-grep-equivalent-cmd-powershell/#:~:text=The%20grep%20command%20in%20Linux,is%20the%20Select%2DString%20command.
-https://stackoverflow.com/questions/1488775/c-remove-new-line-from-multiline-string
-*/
